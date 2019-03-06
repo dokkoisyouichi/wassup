9c20da61df5a01a6787e8b0d47921b7a18b0b144
Keisuke Minami <Keisuke@keisukes-macbook-pro.local>
2019-03-06 22:47:58 +0900

    Initial Commit

---
+++ wassup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wassup
+//
+//  Created by Keisuke Minami on 3/6/31 H.
+//  Copyright © 31 Heisei Keisuke Minami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
